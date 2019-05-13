3d82c26b1deddec0b035d009366086b51dc1ca48
Kelly Street <kstreet@root-pcs-air.dfci.harvard.edu>
2019-05-13 14:56:22 -0400

    modified violin plot to be generic plus methods for matrix and list (should probably add formula?)

--- violinplot.R
+++ violinplot.R
@@ -3,13 +3,14 @@ violinplot <- function (x, ...) {
 }
 
 
-violinplot.matrix <- function(x, col='grey50', names = 1:ncol(x), scale = 1, show.median = TRUE, 
+violinplot.matrix <- function(x, col='grey50', names = 1:ncol(x), show.median = TRUE, 
                        show.box = FALSE, box.big = 5, box.small = 2, bars = FALSE, 
-                       add = FALSE, xlab='', ylab='', width = 1, col.pt = 'black', ...){
+                       add = FALSE, xlab='', ylab='', width = 1, max.width = 1,
+                       col.pt = 'black', ...){
   col <- rep(col, length.out = ncol(x))
   col.pt <- rep(col.pt, length.out = ncol(x))
   width <- rep(width, length.out = ncol(x))
-  width <- width / mean(width)
+  width <- max.width * width / mean(width)
   
   xat <- sapply(1:ncol(x),function(i){
     sum(rep(width,each=2)[1:(2*i-1)])/2
@@ -19,7 +20,7 @@ violinplot.matrix <- function(x, col='grey50', names = 1:ncol(x), scale = 1, sho
   xs <- sapply(dens,function(d){d$x})
   ys <- sapply(dens,function(d){d$y})
   
-  ys <- ys * .5/max(ys,na.rm=TRUE) * scale
+  ys <- ys * .5 * max.width/max(ys,na.rm=TRUE)
   
   if(!add){
     plot(c(xat[1]-.4*width[1],xat[ncol(x)]+.4*width[ncol(x)]), range(xs,na.rm = TRUE), col='white', xaxt = 'n', xlab=xlab, ylab=ylab, ...)
@@ -46,9 +47,10 @@ violinplot.matrix <- function(x, col='grey50', names = 1:ncol(x), scale = 1, sho
 }
 
 
-violinplot.list <- function(x, col='grey50', names = 1:length(x), scale = 1, show.median = TRUE, 
+violinplot.list <- function(x, col='grey50', names = 1:length(x), show.median = TRUE, 
                               show.box = FALSE, box.big = 5, box.small = 2, bars = FALSE, 
-                              add = FALSE, xlab='', ylab='', width = 1, col.pt = 'black', ...){
+                              add = FALSE, xlab='', ylab='', width = 1, max.width = 1,
+                            col.pt = 'black', ...){
   
   col <- rep(col, length.out = length(x))
   col.pt <- rep(col.pt, length.out = length(x))
@@ -63,7 +65,7 @@ violinplot.list <- function(x, col='grey50', names = 1:length(x), scale = 1, sho
   xs <- sapply(dens,function(d){d$x})
   ys <- sapply(dens,function(d){d$y})
   
-  ys <- ys * .5/max(ys,na.rm=TRUE) * scale
+  ys <- ys * .5 * max.width/max(ys,na.rm=TRUE)
   
   if(!add){
     plot(c(xat[1]-.4*width[1],xat[length(x)]+.4*width[length(x)]), range(xs,na.rm = TRUE), col='white', xaxt = 'n', xlab=xlab, ylab=ylab, ...)
